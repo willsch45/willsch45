b197c8210bd689dd2830fddd64f173ea11c0ca6c
willsch45 <will.sch45@gmail.com>
2015-01-27 07:38:01 -0800

    Update

--- Overloaded Methods/src/OverloadedMethods.java
+++ Overloaded Methods/src/OverloadedMethods.java
@@ -5,9 +5,9 @@ public class OverloadedMethods {
 		double[] doubleValues = { 0.1, 3.4, 29.0, 0.4, 0.1, 6.6, 3.4, 0.1, 0.1 };
 
 		System.out.println("Integer mean: " + Mean(integerValues));
-		// System.out.println("Double mean: " + Mean(doubleValues));
-		// System.out.println("Integer median: " + Median(integerValues));
-		// System.out.println("Double median: " + Median(doubleValues));
+		System.out.println("Double mean: " + Mean(doubleValues));
+		System.out.println("Integer median: " + Median(integerValues));
+		System.out.println("Double median: " + Median(doubleValues));
 		// System.out.println("Integer mode: " + Mode(integerValues));
 		// System.out.println("Double mode: " + Mode(doubleValues));
 	}
@@ -93,5 +93,56 @@ public class OverloadedMethods {
 
 	// TODO: overload mean so it works with an array of doubles
 
+	public static double Mean(double[] values) {
+		// don't want to change original array, so make a copy before sorting
+		double[] copy = Copy(values);
+		Sort(copy);
+
+		double sum = 0;
+		for (int position = 0; position < copy.length; position++) {
+			sum += copy[position];
+		}
+
+		double mean = sum / (double) copy.length;
+
+		return mean;
+	}
+
 	// TODO: overload median and mode so that it works with an array of doubles
+
+	public static double Median(int[] values) {
+		double median = 0;
+
+		int[] copy = Copy(values);
+		Sort(copy);
+		int distance;
+
+		if (values.length % 2 == 0) {
+			distance = values.length / 2;
+			median = (values[distance] + values[distance + 1]) / 2;
+		} else {
+			distance = values.length / 2;
+			median = values[distance];
+		}
+
+		return median;
+	}
+
+	public static double Median(double[] values) {
+		double median = 0;
+
+		double[] copy = Copy(values);
+		Sort(copy);
+		int distance;
+
+		if (values.length % 2 == 0) {
+			distance = values.length / 2;
+			median = (values[distance] + values[distance + 1]) / 2;
+		} else {
+			distance = values.length / 2;
+			median = values[distance];
+		}
+
+		return median;
+	}
 }
